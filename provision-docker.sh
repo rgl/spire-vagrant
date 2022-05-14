635ebb69225d8e737d7e94bc5e72828e32e78744
Rui Lopes <rgl@ruilopes.com>
2022-05-14 09:05:35 +0100

    upgrade to docker 20.10.16

--- provision-docker.sh
+++ provision-docker.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 # see https://github.com/moby/moby/releases
-docker_version='20.10.14'
+docker_version='20.10.16'
 
 # prevent apt-get et al from asking questions.
 # NB even with this, you'll still get some warnings that you can ignore:
