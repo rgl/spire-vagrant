b46fbfea07b2cb27ea4364524696382e35a58b9e
Rui Lopes <rgl@ruilopes.com>
2022-07-14 06:56:59 +0100

    upgrade to go 1.18.4

--- provision-go.sh
+++ provision-go.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 # install go.
 # see https://go.dev/dl/
 # see https://go.dev/doc/install
-artifact_url=https://go.dev/dl/go1.18.3.linux-amd64.tar.gz
-artifact_sha=956f8507b302ab0bb747613695cdae10af99bbd39a90cae522b7c0302cc27245
+artifact_url=https://go.dev/dl/go1.18.4.linux-amd64.tar.gz
+artifact_sha=c9b099b68d93f5c5c8a8844a89f8db07eaa58270e3a1e01804f17f4cf8df02f5
 artifact_path="/tmp/$(basename $artifact_url)"
 wget -qO $artifact_path $artifact_url
 if [ "$(sha256sum $artifact_path | awk '{print $1}')" != "$artifact_sha" ]; then
